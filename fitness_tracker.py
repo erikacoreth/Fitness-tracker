dc1a3c398c4c781975bd71060a70d19610e5a588
Erika Coreth <ecoreth@yahoo.com>
2026-01-22 15:59:03 -0500

    Add WorkoutSession class with class-level documentation

---
+++ fitness_tracker.py
@@ -0,0 +1,25 @@
+"""
+fitness_tracker.py
+
+Module for tracking individual fitness workout sessions.
+This module defines the WorkoutSession by calculating the calroies burned 
+based on the duration and intensity of the workout. Then the module summarizes the workout. 
+
+Author: Erika Coreth
+Date: January 2026
+
+
+"""
+
+class WorkoutSession:
+    """
+
+    Represents a single workout session performed by a user.
+
+    A WorkoutSession stores basic information about an exercise, including
+    the name of the exercise, how long it was performed, and the intensity level.
+    It also provides functionality to estimate calories burned and to summarize
+    the workout in a user-friendly format.
+    
+    
+    """
